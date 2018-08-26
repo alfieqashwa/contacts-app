b3418ddb54178e8ff1df75bbbfcd71b8bd284eff
alfieqashwa <alfieqahwa@gmail.com>
2018-08-26 20:08:48 +0700

    finished pure components only

--- src/index.js
+++ src/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Main component. Renders a SearchBar and a ContactList
+class ContactsApp extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ContactList contacts={this.props.contacts} />
+      </div>
+    );
+  }
+}
+ContactsApp.PropTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object)
+};
+
+class SearchBar extends Component {
+  render() {
+    return <input type="search" placeholder="search" />;
+  }
+}
+
+class ContactList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.contacts.map(contact => (
+          <ContactItem
+            key={contact.email}
+            name={contact.name}
+            email={contact.email}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+ContactList.PropTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object)
+};
+
+class ContactItem extends Component {
+  render() {
+    return (
+      <li>
+        {this.props.name} - {this.props.email}
+      </li>
+    );
+  }
+}
+ContactItem.PropTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
+};
+
+let contacts = [
+  { name: "Cassio Zen", email: "cassiozen@gmail.com" },
+  { name: "Dan Abramov", email: "gaearon@somewhere.com" },
+  { name: "Pete Hunt", email: "floydophone@somewhere.com" },
+  { name: "Paul O'Shannessy", email: "zpao@somewhere.com" },
+  { name: "Ryan Florence", email: "rpflorence@somewhere.com" },
+  { name: "Sebastian Markbage", email: "sebmarkbage@somewhere.com" }
+];
+
+render(<ContactsApp contacts={contacts} />, document.getElementById("root"));
 registerServiceWorker();
